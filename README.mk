6bd7cb5ce1bc12b90a7f943fc09ed22ef5299561
Baoxing Song <songbaoxing168@163.com>
2017-09-27 16:36:35 +0200

    Delete README.mk

--- README.mk
+++
@@ -1,5 +0,0 @@
-# Baoxing's CV
-
-Now I am a Ph.D student at MPIPZ. I am developing pipeline for GWAS and natural variantion analysis.
-
-Although not at a very good situation, I hope I will have a good careet future. 
